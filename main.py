42b560e6649a6bf1775785c49b7851fca9ba681b
Reza Anwari <xo22259@bristol.ac.uk>
2024-12-15 21:28:40 +0000

    Calculator first push

---
+++ main.py
@@ -0,0 +1,30 @@
+### Functions
+
+# Add two numbers
+def add(x, y):
+    return x + y
+
+# Subtract two numbers (y from x)
+def subtract(x, y):
+    return x - y
+
+# Multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Divide two numbers
+def divide(x, y):
+    return x / y
+
+def calculator():
+    print("Enter your numbers")
+
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+
+    print("addition: ", round(add(num1, num2), 2))
+    print("Subtraction: ", round(subtract(num1, num2), 2))
+    print("Multiplication: ", round(multiply(num1, num2), 2))
+    print("Division: ", round(divide(num1, num2), 2))
+
+calculator()
